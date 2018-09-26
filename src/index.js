d3a022354c71ad8a45211c8653bed6a3aee06927
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-09-27 07:33:19 +0900

    Set it up test and router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('test for eslint and prettier');
+const express = require('express');
+const logger = require('morgan');
+const router = require('./router');
+
+const app = express();
+const port = 8000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  next();
+});
+
+app.use(logger('dev'));
+
+app.use('/block', router);
+
+app.listen(port, () => console.log(`hello world is waiting for us on port ${port}`));
