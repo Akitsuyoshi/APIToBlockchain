d06bb4196c2e96bc50afa52fb1e7e27189f5a6e6
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-09-28 09:10:53 +0900

    Add stub test for get call

--- src/test.js
+++ src/test.js
@@ -3,10 +3,26 @@ const sinon = require('sinon');
 const request = require('request');
 require('chai').should();
 
+const blocks = require('./mockDataForTest');
+
 const baseURL = 'http://localhost:3000/block';
 
 describe('API', () => {
+  let headerObj;
+  let errObj;
   beforeEach(() => {
+    headerObj = {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json; charset=utf-8',
+        'Content-Length': '238',
+        connection: 'close',
+        'Cache-Control': 'no-cache',
+      },
+    };
+    errObj = {
+      status: 'error', msg: 'the block of given Id does not exist in the chain',
+    };
     this.get = sinon.stub(request, 'get');
     this.post = sinon.stub(request, 'post');
   });
@@ -16,15 +32,7 @@ describe('API', () => {
   });
 
   it('should have header in a correct manner', () => {
-    this.get.yields(null, {
-      statusCode: 200,
-      headers: {
-        'content-type': 'application/json; charset=utf-8',
-        'Content-Length': '238',
-        connection: 'close',
-        'Cache-Control': 'no-cache',
-      },
-    }, JSON.stringify({}));
+    this.get.yields(null, headerObj, JSON.stringify({}));
     request.get(baseURL, (err, res, body) => {
       res.statusCode.should.eql(200);
       res.headers['content-type'].should.contain('application/json; charset=utf-8');
@@ -33,6 +41,45 @@ describe('API', () => {
       res.headers.connection.should.contain('close');
 
       expect(body).to.deep.equal(JSON.stringify({}));
+    });
+  });
+
+  it('should return response 0 height block to get call', () => {
+    this.get.yields(null, headerObj, JSON.stringify(blocks[0]));
+    request.get(`${baseURL}/0`, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      expect(body).to.deep.equal(JSON.stringify({
+        hash: 'a449da79cb962e1ad2f33afd3e60c11ee044ee42feeb1d6d7286bc496bfe2578',
+        height: 0,
+        body: 'First block in the chain - Genesis block',
+        time: '1538090051',
+        previousBlockHash: '',
+      }));
+    });
+  });
+
+  it('should return response, 1 height block to get call', () => {
+    this.get.yields(null, headerObj, JSON.stringify(blocks[1]));
+    request.get(`${baseURL}/1`, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      expect(body).to.deep.equal(JSON.stringify({
+        hash: 'c524497b60a4378e63e134c7c27b1a4431c7d3e3ae4f720348095937cf0bb9b8',
+        height: 1,
+        body: 'Test Block - 1',
+        time: '1538090052',
+        previousBlockHash: 'a449da79cb962e1ad2f33afd3e60c11ee044ee42feeb1d6d7286bc496bfe2578',
+      }));
+    });
+  });
+
+  it('should return response error if block is not existed', () => {
+    headerObj.statusCode = 404;
+    this.get.yields(null, headerObj, JSON.stringify(errObj));
+    request.get(`${baseURL}/12`, (err, res, body) => {
+      res.statusCode.should.equal(404);
+      expect(body).to.deep.equal(JSON.stringify({
+        status: 'error', msg: 'the block of given Id does not exist in the chain',
+      }));
     });
   });
 });
