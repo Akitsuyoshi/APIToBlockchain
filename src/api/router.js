0d48d19644559c7cb70ed267bd925e0c7035c725
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-10-01 08:42:16 +0900

    Add stub test for POST

--- src/api/router.js
+++ src/api/router.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-
 const { getBlockHeight, getBlock } = require('../blockChain/db');
-
 
 router.get('/:blockHeight', async (req, res) => {
   try {
